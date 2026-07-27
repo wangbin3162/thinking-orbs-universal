441fb3c800c52a7caddaca72be68a95280a36297
wangbin <316281400@qq.com>
2026-07-27 14:38:18 +0800

    update：修改打包部署脚本

---
+++ build/util-path.ts
@@ -0,0 +1,7 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export const root = resolve(__dirname, '..')
